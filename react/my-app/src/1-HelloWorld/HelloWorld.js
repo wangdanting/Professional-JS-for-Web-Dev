9c3b120f4c661a33df27e92f28d3486c327ff4da
wangdanting <18875082742@163.com>
2019-01-04 11:04:50 +0800

    learn react docs

---
+++ react/my-app/src/1-HelloWorld/HelloWorld.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+class HelloWorld extends PureComponent {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+export default HelloWorld;
---
+++ react/my-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import HelloWorld from "./1-HelloWorld/HelloWorld";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <HelloWorld />;
+  }
+}
+
+export default App;
