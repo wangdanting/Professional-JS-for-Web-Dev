20477bd6ff94e22c4cbabef260b22d1036ffa28a
wangdanting <18875082742@163.com>
2019-03-13 12:49:55 +0800

    isArrayLikeObject

---
+++ lodash-doc/src/Lang/isArrayLikeObject.js
@@ -0,0 +1,28 @@
+/**
+ * 这个方法类似.isArrayLike，除了它还检查value是否是一个对象
+ * The method is like isArrayLike except that it also checks if value is an object
+ * @param {*} value The value to check
+ * @returns {boolean} Returns true if value is an array-like object
+ *
+ * @example
+ * isArrayLikeObject([1, 2, 3])
+ * // => true
+ *
+ * isArrayLikeObject(document.body.children)
+ * // => true
+ *
+ * isArrayLikeObject('abc')
+ * // => false
+ *
+ * isArrayLikeObject(Function)
+ * // => false
+ */
+
+import isArrayLike from "./isArrayLike.js";
+import isObjectLike from "./isObjectLike.js";
+
+function isArrayLikeObject(value) {
+  return isArrayLike(value) && isObjectLike(value);
+}
+
+export default isArrayLikeObject;
