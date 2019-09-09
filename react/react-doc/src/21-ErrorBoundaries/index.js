b08ed3f53fb9361c45d099d8449ba36c77764338
wangdanting <18875082742@163.com>
2019-09-09 12:55:50 +0800

    补缺漏

--- react/react-doc/src/21-ErrorBoundaries/index.js
+++ react/react-doc/src/21-ErrorBoundaries/index.js
@@ -1,28 +1,28 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent } from "react"
 
 class ErrorBoundaries extends PureComponent {
   state = {
     hasError: false
-  };
+  }
 
   static getDerivedStateFromError(error) {
     return {
       hasError: true
-    };
+    }
   }
 
   componentDidCatch(error, info) {
-    console.log(error, "error");
-    console.log(info, "info");
+    console.log(error, "error")
+    console.log(info, "info")
   }
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+      return <h1>Something went wrong.</h1>
     }
-    const abc = this.props.children;
-    return <div>{abc}</div>;
+    const abc = this.props.children
+    return <div>{abc}</div>
   }
 }
 
-export default ErrorBoundaries;
+export default ErrorBoundaries
--- react/react-doc/src/5-StateAndLifecyle/index.js
+++ react/react-doc/src/5-StateAndLifecyle/index.js
@@ -1,27 +1,31 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent } from "react"
 
 class StateAndLifecyle extends PureComponent {
   static defaultProps = {
     num: 1
-  };
+  }
 
   state = {
     counter: 0
-  };
+  }
 
   componentDidMount() {
-    this.timerID = setInterval(this.tick, 1000);
+    this.timerID = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
   }
 
   tick = () => {
     this.setState((prevState, props) => ({
       counter: prevState.counter + props.num
-    }));
-  };
+    }))
+  }
 
   render() {
-    const { counter } = this.state;
-    return <p>{counter}</p>;
+    const { counter } = this.state
+    return <p>{counter}</p>
   }
 }
-export default StateAndLifecyle;
+export default StateAndLifecyle
